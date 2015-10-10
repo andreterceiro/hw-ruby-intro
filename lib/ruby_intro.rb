6891e4915f08b3cc18dad24d828f5aea3f6d616f
André Terceiro <andreterceiro@yahoo.com.br>
2015-10-10 19:28:54 -0300

    Resolução dos 5 primeiros exercícios

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,24 +4,35 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.reduce(:+).to_i
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr.sort!.reverse.take(2).reduce(:+).to_i
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.size < 2
+  # YOUR CODE HEiRE
+  soma_correta = false 
+  arr.combination(2).map do |c| 
+    soma_correta = true if c.reduce(:+) == n
+  end
+
+  soma_correta
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  
 end
 
 def binary_multiple_of_4? s
